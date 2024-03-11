47ed1e162950846f0fd3510a5e60b79480a037bc
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-03-11 20:03:26 +0530

    ADD. Migration added to rooms, restrictions table.

---
+++ migrations/20240311142952_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General''s Quatres','2024-03-06 00:00:00.000','2024-03-06 00:00:00.000'),
+	 ('Major''s Suite ','2024-03-07 00:00:00.000','2024-03-07 00:00:00.000');
---
+++ migrations/20240311143207_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restrictions_name,created_at,updated_at) VALUES
+	 ('Reservation','2024-03-21 00:00:00.000','2024-03-27 00:00:00.000'),
+	 ('Owner Block ','2024-03-28 00:00:00.000','2024-03-28 00:00:00.000');
